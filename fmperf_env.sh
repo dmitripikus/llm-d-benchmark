27513b24f5dc814627a71b8be2d4550694ee7fe8
Dmitri Pikus <dpikus@il.ibm.com>
2025-09-01 09:38:13 +0300

    Environment configurations for inference-perf and fmperf

---
+++ fmperf_env.sh
@@ -0,0 +1,8 @@
+export LLMDBENCH_CONTROL_WORK_DIR=/Users/dpikus/DP_FILES/Data/projects/llm-d/kv-cache-benchmarking/LLM-D-BENCHMARK/fmperf
+export LLMDBENCH_HF_TOKEN=$(oc get secrets llm-d-hf-token -o jsonpath='{.data.*}' | base64 -d)
+export LLMDBENCH_VLLM_COMMON_PVC_NAME=dima-hash-fix-chart-llama-3-70b-instruct-storage-claim
+
+export LLMDBENCH_IMAGE_REGISTRY=quay.io
+export LLMDBENCH_IMAGE_REPO="dpikus"
+export LLMDBENCH_IMAGE_NAME="llm-d-benchmark"
+export LLMDBENCH_IMAGE_TAG="v0.2.2_fix"
