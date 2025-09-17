c0c2623282ff84bdfe3db7c368362b6f46b8237b
Dmitri Pikus <dpikus@il.ibm.com>
2025-09-17 10:47:05 +0300

    More inf_perf scenarios are added

--- util/run_all_scenarios_inf_perf.sh
+++ util/run_all_scenarios_inf_perf.sh
@@ -2,16 +2,49 @@
 #epps=(estimate)
 
 # Scheduler experiment 2
-#epps=(sched_random shed_load_kvcache_util sched_estimate sched_load_dominant sched_estimate_dominant)
-#workloads=(sched_synthetic)
+# epps=(sched_random shed_load_kvcache_util shed_load_only shed_active_req_kvcache_util)
+# workloads=(sched_synthetic)
+
+
+# Scheduler experiment - prefix-only vs prefix+load, with shared prefix, long input
+# epps=(sched_estimate sched_estimate_load)
+# workloads=(shared_prefix_synthetic_long_input)
+
+
+# Scheduler experiment - prefix-only with shared prefix, long input (TEMP run - for logs only)
+# epps=(sched_estimate)
+# workloads=(shared_prefix_synthetic_long_input)
+
+# Scheduler experiment - prefix+load with shared prefix, long input (TEMP run - for logs only)
+# epps=(sched_estimate_load)
+# workloads=(shared_prefix_synthetic_long_input)
+
+# Scheduler experiment - prefix-only(precise) with shared prefix, long input (TEMP run - for logs only)
+#epps=(sched_cache_tracking_only sched_cache_tracking_load)
+#epps=(sched_cache_tracking_only)
+# epps=(sched_cache_tracking_load)
+# workloads=(shared_prefix_synthetic_long_input2)
+
+
+# Scheduler experiment - prefix-only vs prefix+load, with NO shared prefix, long input
+#epps=(sched_cache_tracking_only sched_cache_tracking_load)
+epps=(sched_cache_tracking_load)
+workloads=(sched_random_constant3)
+
+# Scheduler experiment 3
+# epps=(sched_random shed_load_only)
+# workloads=(sched_synthetic_modified)
+
+
+
 
 # Scheduler - new load scorer
 # epps=(sched_random shed_active_req_kvcache_util)
 # workloads=(sched_synthetic)
 
 # Scheduler - load only (GIE impl)
-epps=(sched_random shed_load_only)
-workloads=(sched_synthetic)
+# epps=(sched_random shed_load_only)
+# workloads=(sched_synthetic)
 
 
 #workloads=(shared_prefix_synthetic_exp1 shared_prefix_synthetic_exp2 shared_prefix_synthetic_exp3)
